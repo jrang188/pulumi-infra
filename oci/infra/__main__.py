3289d5f2c915cb0997d7a1e5b33762b52dfb99cd
Justin Ang <justinang177@gmail.com>
2025-03-29 16:57:18 -0700

    remove private subnet

--- oci/infra/__main__.py
+++ oci/infra/__main__.py
@@ -1,4 +1,5 @@
 """A OCI Python Base Infra Pulumi program"""
+
 import pulumi
 from network import Network
 
@@ -10,9 +11,3 @@ config = pulumi.Config()
 
 # Instantiate the Network class to create network resources.
 network = Network(oci_config, config)
-
-# Outputs
-pulumi.export("vcn_id", network.vcn.id)
-pulumi.export("private_subnet_id", network.private_subnet.id)
-pulumi.export("public_subnet_id", network.public_subnet.id)
-pulumi.export("internet_gateway_id", network.internet_gateway.id)
