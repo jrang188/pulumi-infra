b32acd4ce23211738711eb0c89cc67d382993457
Justin Ang <justinang177@gmail.com>
2025-03-29 17:27:37 -0700

    add k3s vm

--- oci/infra/__main__.py
+++ oci/infra/__main__.py
@@ -2,6 +2,7 @@
 
 import pulumi
 from network import Network
+from compute import ComputeInstance
 
 # -----------------------------------------------------------
 # Configuration
@@ -11,3 +12,14 @@ config = pulumi.Config()
 
 # Instantiate the Network class to create network resources.
 network = Network(oci_config, config)
+
+# Instantiate the ComputeInstance class to create ARM-based VM for K3S Server
+k3s_server = ComputeInstance(
+    oci_config,
+    config,
+    instance_name="k3s-server",
+    instance_ocpus=2,
+    instance_memory_in_gbs=12,
+    subnet_id=network.public_subnet.id,
+    amd64_cpu=False,  # Use ARM-based VM for K3S Server
+).output()
